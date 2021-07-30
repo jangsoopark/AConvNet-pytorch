4b5e3d5a45aa6b768d23784c245cb46c0798e14d
jangsoopark <jspark@ivstech.co.kr>
2021-07-30 14:03:21 +0900

    target chip

--- src/data/generate_dataset.py
+++ src/data/generate_dataset.py
@@ -51,8 +51,14 @@ def generate(src_path, dst_path, is_train, chip_size, patch_size, use_phase, dat
             if not use_phase:
                 _image = np.abs(_image)
                 _image = 20 * np.log10(500 * _image + 3)
+                _image = data_scaling(_image)
+            else:
+                _image[:, :, 0] = np.abs(_image[:, :, 0])
+                _image[:, :, 0] = 20 * np.log10(500 * _image[:, :, 0] + 3)
 
-            _image = data_scaling(_image)
+                _image[:, :, 0] = data_scaling(_image[:, :, 0])
+                _image[:, :, 1] = data_scaling(_image[:, :, 1])
+
             np.save(os.path.join(dst_path, f'{name}-{i}.npy'), _image)
             # Image.fromarray(data_scaling(_image)).convert('L').save(os.path.join(dst_path, f'{name}-{i}.bmp'))
 
